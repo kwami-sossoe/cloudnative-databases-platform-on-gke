3a490f39c0eef4567f1807fe23c1bb38c9d150ae
Kwami S. Sossoe <kwami.sossoe@gmail.com>
2024-12-31 19:16:46 +0000

    cnpg helm package in artifact registry

--- manifests/tooling/cnpg/cnpg-helm-in-ar.sh
+++ manifests/tooling/cnpg/cnpg-helm-in-ar.sh
@@ -1,16 +1,20 @@
 export region=europe-west1 
-export gke_hub_project_id=ops-152437-anthos-dv
-export artifact_registry_project_id=cloud-insights-data-lab
-export clustername=cloud-native-db-prod-cluster
+export gke_hub_project_id=""
+export artifact_registry_project_id=""
+export clustername=""
 export artifact_registy_repo=cndb-registry
 
-cd /home/iac/dev-charm/skwailab-accelerator/cloud-native-databases/db-helm-charts/
+git clone https://github.com/cloudnative-pg/charts.git 
 
-helm package cloudnative-pg-helm/charts/cluster/
+mv charts cnpg-helm
+
+cd cnpg-helm 
+
+helm package cnpg-helm/charts/cloudnative-pg .
 
 gcloud --project=$artifact_registry_project_id auth print-access-token | helm registry login -u oauth2accesstoken --password-stdin https://$region-docker.pkg.dev
 
-helm push cluster-0.0.9.tgz oci://$region-docker.pkg.dev/$artifact_registry_project_id/$artifact_registy_repo/cluster
+helm push cloudnative-pg-0.23.0.tgz oci://$region-docker.pkg.dev/$artifact_registry_project_id/$artifact_registy_repo/cloudnative-pg
 
 gcloud --project=$artifact_registry_project_id artifacts docker images list $region-docker.pkg.dev/$artifact_registry_project_id/$artifact_registy_repo --include-tags
 
